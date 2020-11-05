316d3a041a911f8e53af358a5246fdc27f0aa101
smittyy <57191850+smittyy@users.noreply.github.com>
2020-11-05 09:13:20 -0500

    Add project files.

---
+++ XMLSerialization/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMLSerialization
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Design { get; set; }
+        public int Research { get; set; }
+        public int Software { get; set; }
+
+        public DateTime EntryDate {get; set;}
+    }
+}
---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Josh = new Employee();
+
+            Josh.Name = "Josh Willy";
+            Josh.Speed = 4;
+            Josh.Design = 50;
+            Josh.Research = 70;
+            Josh.Software = 120;
+            Josh.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"His name is {Josh.Name}, his speed is {Josh.Speed}. \nHis Disign, Research, and Software are {Josh.Design}, {Josh.Research}, {Josh.Software} is that order. \nHe joined on {Josh.EntryDate}");
+
+            string FilePath = "C:/Users/Smith_Conner/Desktop/C#/Serialization/XMLSerialization/XMLSerialization/bin/Debug/";
+            string FileName = "Serial.xml";
+            XmlSerializer nuke = new XmlSerializer(typeof(Employee));
+            using (TextWriter ser = new StreamWriter(FilePath + FileName, append: false))
+            {
+                nuke.Serialize(ser, Josh);
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
